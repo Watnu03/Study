77eed7ab4baed51493d9d56dcd3c95ed7e3d0809
watnu <watnu03@gmail.com>
2025-02-13 23:01:35 +0900

    ✨ day18: useParams 해보기

--- src/pages/Edit.jsx
+++ src/pages/Edit.jsx
@@ -1,7 +1,11 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 const Edit = () => {
-  return <div>Edit</div>;
+  const { id } = useParams();
+
+  console.log(id); // id is 1234567890
+  return <h1>Edit 페이지는 {id}번 일기입니다</h1>;
 };
 
 export default Edit;
