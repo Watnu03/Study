2ea438b4c86a718567539c913777027ff6ddd2e1
watnu <watnu03@gmail.com>
2025-02-15 23:20:16 +0900

    ✨ Header에 Click 이벤트 props 추가

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,24 @@
 import Button from "./Button";
 import React from "react";
 
-const Header = ({ left, leftStatus, title, right, rightStatus }) => {
+const Header = ({
+  left,
+  leftStatus,
+  onClickLeft,
+  title,
+  right,
+  rightStatus,
+  onClickRight,
+}) => {
   return (
     <div className="header">
       <div className="header_left">
-        <Button text={left} status={leftStatus} />
+        <Button text={left} status={leftStatus} onClick={onClickLeft} />
       </div>
       <div className="header_center">{title}</div>
       {right ? (
         <div className="header_right">
-          <Button text={right} status={rightStatus} />
+          <Button text={right} status={rightStatus} onClick={onClickRight} />
         </div>
       ) : null}
     </div>
