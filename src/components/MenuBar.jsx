ada2611148f8b30c8bf046f72488e57fe8b03eae
watnu <watnu03@gmail.com>
2025-02-15 23:22:31 +0900

    ✨ 정렬기능 추가(최신 순, 오래된 순)

--- src/components/MenuBar.jsx
+++ src/components/MenuBar.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 
-const MenuBar = ({ handleCreateBtn }) => {
+const MenuBar = ({ onSortItems, onClickCreateBtn }) => {
   return (
     <div className="menu_bar">
-      <select name="filter" id="">
+      <select
+        name="filterItems"
+        onChange={(e) => {
+          onSortItems(e.target.value);
+        }}
+      >
         <option value="latest">최신순</option>
         <option value="oldest">오래된 순</option>
       </select>
-      <button onClick={handleCreateBtn} className="button button-create">
+      <button onClick={onClickCreateBtn} className="button button-create">
         새 일기 쓰기
       </button>
     </div>
