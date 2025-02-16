8d06cc0af4e2d93144435ed8eea2608e197dd231
watnu <watnu03@gmail.com>
2025-02-16 17:30:49 +0900

    ♻️ 공통부분 분리 / 내용 Viewer 컴퍼넌트로 분리

--- src/pages/Diary.jsx
+++ src/pages/Diary.jsx
@@ -1,76 +1,34 @@
-import { useContext, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 
-import { DiaryStateContext } from "../App";
 import Header from "../components/Header";
-import getEmotionImage from "../util/get-emotion-img";
-
-const emotionData = [
-  { emotionId: 1, emotionName: "완전좋음" },
-  { emotionId: 2, emotionName: "좋음" },
-  { emotionId: 3, emotionName: "그럭저럭" },
-  { emotionId: 4, emotionName: "나쁨" },
-  { emotionId: 5, emotionName: "끔찍함" },
-];
+import Viewer from "../components/Viewer";
+import { getStringedDate } from "../util/get-stringed-date";
+import useDiary from "../hooks/useDiary";
 
 const Diary = () => {
   const navigate = useNavigate();
   const params = useParams();
-  const data = useContext(DiaryStateContext);
+  const currentDiaryItem = useDiary(params.id);
 
-  const [currentDiaryItem, setCurrentDiaryItem] = useState({});
-  const [currentDiaryDate, setCurrentDiaryDate] = useState();
+  if (!currentDiaryItem) {
+    return <div>로딩중...</div>;
+  }
 
-  useEffect(() => {
-    const initDiaryItem = data.find(
-      (item) => String(item.id) === String(params.id)
-    );
+  const { date, emotionId, content } = currentDiaryItem;
 
-    if (initDiaryItem) {
-      setCurrentDiaryDate(
-        new Date(Number(initDiaryItem.date)).toISOString().slice(0, 10)
-      );
-    }
-    setCurrentDiaryItem(initDiaryItem);
-  }, [params.id, currentDiaryDate]);
+  const title = getStringedDate(new Date(date));
 
   return (
     <div className="diary">
       <Header
         left={"< 뒤로가기"}
         onClickLeft={() => navigate("/")}
-        title={`${currentDiaryDate} 기록`}
+        title={`${title} 기록`}
         right={"수정하기"}
         onClickRight={() => navigate(`/edit/${params.id}`)}
       />
-      <div className="diary_content">
-        <section className="td_emotion">
-          <h4>오늘의 감정</h4>
-          <div
-            className={`emotionImage ${
-              currentDiaryItem.emotionId
-                ? `emotion-${currentDiaryItem.emotionId}`
-                : ""
-            }`}
-          >
-            <img
-              src={getEmotionImage(currentDiaryItem.emotionId)}
-              alt={`emotion-${currentDiaryItem.emotionId}`}
-            />
-            <p>
-              {emotionData.find(
-                (item) => item.emotionId === currentDiaryItem?.emotionId
-              )?.emotionName || ""}
-            </p>
-          </div>
-        </section>
-        <section className="td_content">
-          <h4>오늘의 일기</h4>
-          <div className="content">
-            <p>{currentDiaryItem.content}</p>
-          </div>
-        </section>
-      </div>
+      <Viewer emotionId={emotionId} content={content} />
     </div>
   );
 };
