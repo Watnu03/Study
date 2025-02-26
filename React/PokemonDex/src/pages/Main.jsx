bc11f5e2a964e1704e8b04183fa094ad986b1d75
watnu <watnu03@gmail.com>
2025-02-27 00:13:55 +0900

    💄 Search bar 위치 조정

--- React/PokemonDex/src/pages/Main.jsx
+++ React/PokemonDex/src/pages/Main.jsx
@@ -1,17 +1,43 @@
+import React, { useState } from "react";
+
+import DetailModal from "../components/modal/DetailModal";
 import FilterMenu from "../components/common/FilterMenu";
 import PokeList from "../components/pokeComponents/PokeList";
-import React from "react";
+import SearchBar from "../components/common/SearchBar";
 import styled from "styled-components";
 
 const Main = () => {
+  const [closeModal, setCloseModal] = useState(false);
+  const [selectPokemon, setSelectPokemon] = useState(null);
+
+  const onOpenModal = (pokemonId) => {
+    setSelectPokemon(pokemonId);
+    setCloseModal(true);
+  };
+
+  const onCloseModal = () => {
+    setSelectPokemon(null);
+    setCloseModal(false);
+  };
+
   return (
-    <MainWrapper>
-      <FilterMenu />
-      <PokeList />
-    </MainWrapper>
+    <>
+      <div>
+        <SearchBar />
+        <MainWrapper>
+          <FilterMenu />
+          <PokeList onOpenModal={onOpenModal} />
+        </MainWrapper>
+      </div>
+      {closeModal && (
+        <DetailModal pokemon={selectPokemon} onCloseModal={onCloseModal} />
+      )}
+    </>
   );
 };
 
 export default Main;
 
-const MainWrapper = styled.div``;
+const MainWrapper = styled.div`
+  padding: 0 25px;
+`;
