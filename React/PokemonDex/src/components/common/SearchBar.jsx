0396538c49f2215ebf6f112559f8cc0e14a3c5c5
watnu <watnu03@gmail.com>
2025-03-01 18:13:17 +0900

    ✨ 검색 기능 추가

--- React/PokemonDex/src/components/common/SearchBar.jsx
+++ React/PokemonDex/src/components/common/SearchBar.jsx
@@ -1,13 +1,43 @@
+import React, { useState } from "react";
+
 import { IoSearchOutline } from "react-icons/io5";
-import React from "react";
 import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
 
 const SearchBar = () => {
+  const nav = useNavigate();
+  /*NOTE: searchParams.get("q") =>URL에 있는 쿼리스트링중 q 값 가져옴
+    URL이 /search?q=Pokemon이면 searchParams.get("q")는 Pokemon을 반환
+  */
+  const [query, setQuery] = useState("");
+
+  const handleChangeSearch = (e) => {
+    // 사용자가 입력하는 검색어를 상태에 저장
+    setQuery(e.target.value);
+  };
+  //  Enter 키 입력시
+  const handleEnterSearch = (e) => {
+    if (e.key === "Enter") {
+      nav(`/search?q=${query}`);
+    }
+  };
+
+  // 검색버튼 클릭시
+  const handleClickSearch = () => {
+    nav(`/search?q=${query}`);
+  };
+
   return (
     <SearchBarWrapper>
       <InputWrapper>
-        <input type="text" placeholder="검색하실 포켓몬이름을 입력해주세요.." />
-        <SearchButton />
+        <input
+          type="text"
+          placeholder="검색하실 포켓몬이름을 입력해주세요.."
+          value={query}
+          onChange={handleChangeSearch}
+          onKeyDown={handleEnterSearch}
+        />
+        <SearchButton onClick={handleClickSearch} />
       </InputWrapper>
     </SearchBarWrapper>
   );
--- React/PokemonDex/src/pages/Search.jsx
+++ React/PokemonDex/src/pages/Search.jsx
@@ -1,7 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
+
+import DetailModal from "../components/modal/DetailModal";
+import PokemonList from "../components/pokemon/PokemonList";
+import styled from "styled-components";
+import { useSearchParams } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const Search = () => {
-  return <div>Search</div>;
+  const [searchParams] = useSearchParams();
+  // 쿼리 스트링에서 검색어 가져오기
+  const searchQuery = searchParams.get("q") || "";
+
+  // 리덕스 상태에서 pokemonData를 가져오기
+  const pokemonsData = useSelector((el) => el.pokemon.pokemonData);
+
+  // 검색어가 포함된 포켓몬 찾기
+  const resultPokemon = pokemonsData.filter((el) =>
+    // 검색어 포함 여부 체크
+    el.name.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
+  const [closeModal, setCloseModal] = useState(false);
+  const [selectPokemon, setSelectPokemon] = useState(null);
+
+  const onOpenModal = (pokemonId) => {
+    setSelectPokemon(pokemonId);
+    setCloseModal(true);
+  };
+
+  const onCloseModal = () => {
+    setSelectPokemon(null);
+    setCloseModal(false);
+  };
+
+  return (
+    <>
+      <SearchWrapper>
+        <h3>" {searchQuery} "으로 검색 한 결과</h3>
+        <PokemonList pokemonsData={resultPokemon} onOpenModal={onOpenModal} />
+      </SearchWrapper>
+      {closeModal && (
+        <DetailModal pokemon={selectPokemon} onCloseModal={onCloseModal} />
+      )}
+    </>
+  );
 };
 
 export default Search;
+
+const SearchWrapper = styled.div`
+  padding: 0 25px;
+  h3 {
+    padding-top: 20px;
+    margin-left: 10px;
+  }
+`;
