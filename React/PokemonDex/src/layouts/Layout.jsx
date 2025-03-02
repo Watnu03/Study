85fe702207045fbe52917dfb1d0cb40363fcf256
watnu <watnu03@gmail.com>
2025-03-02 16:34:31 +0900

    💄 스크롤 디자인 추가

--- React/PokemonDex/src/layouts/Layout.jsx
+++ React/PokemonDex/src/layouts/Layout.jsx
@@ -16,4 +16,18 @@ const Layout = () => {
 
 export default Layout;
 
-const OutletWrapper = styled.div``;
+const OutletWrapper = styled.div`
+  height: calc(100vh - 140px);
+  overflow-y: scroll;
+
+  &::-webkit-scrollbar {
+    width: 13px;
+    background-color: #fff;
+  }
+  &::-webkit-scrollbar-thumb {
+    background-color: #ff6347;
+  }
+  &::-webkit-scrollbar-track {
+    background-color: #fff;
+  }
+`;
