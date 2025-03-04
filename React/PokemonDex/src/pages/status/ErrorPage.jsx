eb667b8e71011b915f58b2d4080b2053f62cff38
watnu <watnu03@gmail.com>
2025-03-04 23:39:32 +0900

    💄 Error,Loading ui 제작

--- React/PokemonDex/src/pages/status/ErrorPage.jsx
+++ React/PokemonDex/src/pages/status/ErrorPage.jsx
@@ -1,7 +1,85 @@
-import React from "react";
+import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
 
 const ErrorPage = () => {
-  return <div>ErrorPage</div>;
+  const nav = useNavigate();
+
+  return (
+    <NotFoundWrapper>
+      <div className="title">
+        <h1>4</h1>
+        <div className="img-box">
+          <img src="/error.gif"></img>
+        </div>
+        <h1>4</h1>
+      </div>
+      <div className="description">
+        <h2>페이지를 찾을 수 없습니다</h2>
+        <p>주소가 잘못되었거나 더 이상 제공되지 않는 페이지 입니다.</p>
+        <button onClick={() => nav("/")}>메인페이지로 돌아가기</button>
+      </div>
+    </NotFoundWrapper>
+  );
 };
 
 export default ErrorPage;
+
+const NotFoundWrapper = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  z-index: 1001;
+
+  .title {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    h1 {
+      font-size: 150px;
+      color: #e44e4e;
+    }
+    .img-box {
+      width: 150px;
+      height: 150px;
+      margin: 0 30px 0 0;
+      border: 25px solid #e44e4e;
+      border-radius: 50%;
+      object-fit: cover;
+      overflow: hidden;
+
+      img {
+        width: 120%;
+      }
+    }
+  }
+
+  .description {
+    text-align: center;
+    h2 {
+      padding-top: 10px;
+      font-size: 2rem;
+    }
+    p {
+      font-size: 1rem;
+      padding: 10px 0;
+      color: #797979;
+    }
+    button {
+      outline: none;
+      border: none;
+      border-radius: 15px;
+      background-color: #e44e4e;
+      color: #fff;
+      cursor: pointer;
+      padding: 8px 15px;
+      margin: 10px 0;
+
+      &:hover {
+        background-color: #bc4d4d;
+      }
+    }
+  }
+`;
--- React/PokemonDex/src/pages/status/LoadingPage.jsx
+++ React/PokemonDex/src/pages/status/LoadingPage.jsx
@@ -1,7 +1,71 @@
 import React from "react";
+import styled from "styled-components";
 
 const LoadingPage = () => {
-  return <div>LoadingPage</div>;
+  return (
+    <LoadingWrapper>
+      <img src="/assets/icon-able.png"></img>
+      <img src="/assets/icon-able.png"></img>
+      <img src="/assets/icon-able.png"></img>
+    </LoadingWrapper>
+  );
 };
 
 export default LoadingPage;
+
+const LoadingWrapper = styled.div`
+  height: 100vh;
+  background-color: rgb(249, 249, 249, 0.6);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  z-index: 1001;
+
+  img {
+    margin: 0 5px;
+    width: 23px;
+  }
+
+  @keyframes bounce1 {
+    0%,
+    33% {
+      transform: translateY(0);
+    }
+    16% {
+      transform: translateY(-20px);
+    }
+  }
+
+  @keyframes bounce2 {
+    33%,
+    66% {
+      transform: translateY(0);
+    }
+    50% {
+      transform: translateY(-20px);
+    }
+  }
+
+  @keyframes bounce3 {
+    66%,
+    100% {
+      transform: translateY(0);
+    }
+    83% {
+      transform: translateY(-20px);
+    }
+  }
+
+  img:first-child {
+    animation: bounce1 2s ease-in-out infinite;
+  }
+
+  img:nth-child(2) {
+    animation: bounce2 2s ease-in-out infinite;
+  }
+
+  img:last-child {
+    animation: bounce3 2s ease-in-out infinite;
+  }
+`;
